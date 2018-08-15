2a44db830ebef3db458e6ba1611c762245e143b3
Thilagavathi1 <rthilagavathi305@gmail.com>
2018-08-15 14:08:43 +0600

    openalpr integrated for android

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -12,47 +13,72 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+import Camera from 'react-native-openalpr';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  textContainer: {
+    position: 'absolute',
+    top: 100,
+    left: 50,
+  },
+  text: {
+    textAlign: 'center',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  preview:{
+    flex:1,
+
+  }
 });
+type Props = {};
+export default class App extends Component<Props> {
+    constructor(props) {
+    super(props);
+
+    this.camera = null;
+    this.state = {
+      camera: {
+        aspect: Camera.constants.Aspect.fill,
+      },
+      plate: 'Scan a plate',
+    };
+  }
+
+
+  onPlateRecognized = ({ plate, confidence }) => {
+    if (confidence > 90) {
+      this.setState({
+        plate,
+      })
+    }
+  }
+   render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={this.state.camera.aspect}
+          captureQuality={Camera.constants.CaptureQuality.medium}
+          country="us"
+          onPlateRecognized={this.onPlateRecognized}
+          plateOutlineColor="#ff0000"
+          showPlateOutline
+          torchMode={Camera.constants.TorchMode.off}
+          touchToFocus
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{this.state.plate}</Text>
+        </View>
+      </View>
+
+    );
+  }
+}
+
+
